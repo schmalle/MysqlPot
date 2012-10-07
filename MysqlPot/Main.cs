40d2ed3412d1e8069c84f5f2ca93eb02b95c3685
Markus Schmall <markus@mschmall.de>
2012-10-07 19:56:57 +0200

    added extened command line parameters

--- MysqlPot/Main.cs
+++ MysqlPot/Main.cs
@@ -47,25 +47,23 @@ namespace MysqlPot
 					{
 						ip = args[2];	
 					}				
-				
+
+					if (args.Length >= 4)
+					{
+						username = args[3];	
+					}
+
+					if (args.Length >= 5)
+					{
+						token = args[4];	
+					}
+
 				}						
 			
 			}
 
-
-			//
-			// parse commandline
-			//
-			// programm name_of_configfile
-			//
-			foreach( string s in args )
-			{
-				String parameter = s;
-				Console.WriteLine(s);
-			}
-
 			Console.WriteLine ("Starting mysql pot with port " + port + " and logfile " + logFile + " and IP " + ip);
-			Console.WriteLine ("Command line parameters: port(3306 default), logfile(" + Config.LogFilename + " default) and IP to bind to...");
+			Console.WriteLine ("Command line parameters: port logfile ip username password host");
 			MySqlServer mysqlServer = new MySqlServer(port, logFile, ip);
 			mysqlServer.start(username, token, host);
 
@@ -86,11 +84,12 @@ namespace MysqlPot
  		* 
  		**/
    		public static int Port {
-       		get { 
-				int port;
-				if (!int.TryParse(ConfigurationManager.AppSettings["mysql.port"], out port)){
+       		get 
+			{ 
+				int port = 3306;
+				if (!int.TryParse(ConfigurationManager.AppSettings["mysql.port"], out port))
+                {
 					// no entry found or conversion not succeeded, use default
-					port = 3306;
 				}
 				return port; 
 			}
@@ -101,10 +100,13 @@ namespace MysqlPot
  		*	IP to bind on
  		* 
  		**/
-   		public static String IP {
-       		get { 
+   		public static String IP 
+		{
+       		get 
+			{ 
 				String value = ConfigurationManager.AppSettings["mysql.ip"];
-				if (value == null){
+				if (value == null)
+				{
 					// no entry found, use default
 					value = "127.0.0.1";
 				}
@@ -118,10 +120,13 @@ namespace MysqlPot
  		*	full qualified filename for log entries
  		* 
  		**/
-   		public static String LogFilename {
-       		get { 
+   		public static String LogFilename 
+        {
+       		get 
+			{ 
 				String value = ConfigurationManager.AppSettings["mysql.log"];
-				if (value == null){
+				if (value == null)
+				{
 					// no entry found, use default
 					value = "/Users/flake/mysqlpot.log";
 				}
@@ -135,12 +140,13 @@ namespace MysqlPot
  		*	Host name, will be used as target for Alerts
  		* 
  		**/
-   		public static String Host {
+   		public static String Host 
+        {
        		get { 
 				String value = ConfigurationManager.AppSettings["mysql.host"];
 				if (value == null){
 					// no entry found, use default
-					value = "www.emailplaceng.de";
+					value = "<no_host>";
 				}
 				return value; 
 			}
@@ -151,12 +157,14 @@ namespace MysqlPot
  		*	Host name, will be used as target for Alerts
  		* 
  		**/
-   		public static String Username {
+   		public static String Username 
+        {
        		get { 
 				String value = ConfigurationManager.AppSettings["server.username"];
-				if (value == null){
+				if (value == null)
+				{
 					// no entry found, use default
-					value = "MYSQL-EMAILPLACE";
+					value = "<user-name>";
 				}
 				return value; 
 			}
@@ -172,7 +180,7 @@ namespace MysqlPot
 				String value = ConfigurationManager.AppSettings["server.token"];
 				if (value == null){
 					// no entry found, use default
-					value = "blah";
+					value = "<password>";
 				}
 				return value; 
 			}
@@ -183,10 +191,12 @@ namespace MysqlPot
  		*	URL for posting Alerts
  		* 
  		**/
-   		public static String ServerPostAlertsURL {
+   		public static String ServerPostAlertsURL 
+        {
        		get { 
 				String url = ConfigurationManager.AppSettings["server.postAlertsURL"];
-				if (url == null){
+				if (url == null)
+				{
 					// no entry found, use default
 					url = "https://www.t-sec-radar.de:9443/ews-0.1/alert/postSimpleMessage";
 				}
